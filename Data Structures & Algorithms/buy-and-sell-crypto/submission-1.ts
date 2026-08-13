f8b26ed3b5765240edbe88cef9cb728bd3b6ad83
Nitan Jana <67586363+nitan-jana@users.noreply.github.com>
2026-08-13 08:47:43 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = prices[0],
+            maxProfit = 0;
+
+        for (let i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            const profit = prices[i] - minPrice;
+            maxProfit = Math.max(profit, maxProfit);
+        }
+
+        return maxProfit;
+    }
+}
