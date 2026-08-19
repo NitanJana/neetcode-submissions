32cdabc0dd3b8e6d34ef3aa212257768b3a196ad
Nitan Jana <67586363+nitan-jana@users.noreply.github.com>
2026-08-19 10:34:28 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        let l = 0,
+            r = s.length - 1;
+
+        while (l < r) {
+            while (l < r && !s[l].match(/[a-z0-9]/i)) {
+                l++;
+            }
+            while (l < r && !s[r].match(/[a-z0-9]/i)) {
+                r--;
+            }
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
